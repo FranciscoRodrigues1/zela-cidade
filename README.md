5f0649e6a2bce70c3c3942a0b60a78f56f0e2738
FranciscoRodrigues1 <franciscorodrigues.dev@gmail.com>
2026-03-30 19:25:11 -0300

    configura CORS e porta dinâmica para deploy

---
+++ README.md
@@ -0,0 +1,83 @@
+# 🚀API ZelaCidade
+## 📌 Sobre o Projeto
+A API **ZelaCidade** foi criada para registrar e gerenciar projetos urbanos, como:
+
+- Buracos
+- Vazamentos
+- Lixo
+- Iluminação
+
+Essa API nos permite criar, visualizar, atualizar e deletar ocorrências.
+
+## 🛠️ Tecnologias utilizadas
+
+- Node
+- Express
+- SQlite
+- SQlite3
+- Postman
+- Nodemon
+
+---
+## 📦 Instalação
+`npm install`
+
+## ▶️ Como Executar
+
+```bash
+npm run dev
+```
+`http://localhost:3000`
+
+[Clique Aqui](http://lacalhost:3000)
+
+---
+
+## 🗄️ Banco de Dados
+
+O banco de dados é criado automaticamente ao iniciar o projeto.
+
+```
+database.db
+```
+
+## 🧾 Tabela
+
+|Campo      | Descrição |
+|-----------| ----------|
+|id         |Identificador único|
+|tipo_problema|Tipo do problema |
+
+
+
+<!-- ## Esses emojis é um padrão em praticamente TODO README:
+
+## 🚀 Nome da API / Projeto
+## 📌 Sobre o Projeto
+## 🎯 Objetivo
+## 🛠️ Tecnologias
+## 📦 Instalação
+## ▶️ Como Executar
+## ⚙️ Configurações
+## 🗄️ Banco de Dados
+## 🔗 Endpoints
+## 🔐 Segurança
+## 📚 Conceitos
+## 💡Dicas / Melhorias
+## 👩‍💻 Autor
+---
+## 📖 Descrição
+## 🔧 Ferramentas
+## 💻 Ambiente
+## 📊 Dados
+## 🧾 Tabela
+## 📡 Requisições
+## 📥 Entrada de dados
+## 📤 Saída de dados
+## 🚫 Bloqueios / proteção
+## 🧠 Aprendizado
+## 🎓 Educacional
+## ⚠️ Atenção
+## ❗Importante
+## 🤝 Contribuição
+## 📄 Licença -->
--- server.js
+++ server.js
@@ -3,7 +3,11 @@
 const express = require("express"); //Framework que cria o servidor e as rotas
 const { criarBanco } = require("./database"); //A chave que vai abrir a conexão com o banco de dados
 
+const cors = require("cors");//importando pacote que gerencia as informaçõe de acesso
+
 const app = express(); //Inicialização: Ligando o motor do servidor
+
+app.use(cors())//ativando 0 CORS no servidor
 
 app.use(express.json()); //Tradutor: Configura o Express para entender dados enviados no formato JSON
 
@@ -13,60 +17,126 @@ app.get("/", (req, res) => {
   //res.send: Envia uma resposta simples (texto, html, json)
   res.send(`
         <body>
-    <h1> ZelaCidade </h1>
-    <h2> Gestão de Problemas Urbanos </h2>
-    <p> Endpoint que leva aos incidentes cadastrados: /incidentes </p> 
+          <h1> ZelaCidade </h1>
+          <h2> Gestão de Problemas Urbanos </h2>
+          <p> Endpoint que leva aos incidentes cadastrados: /incidentes </p> 
         </body>
         
         `);
 });
 
-//Porta do servidor
 
-const PORT = 3000;
 
-//Ligando o Servidor
-app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
 
 
 //Rota de Listagem - Para buscar todos os problemas registrados
 
 app.get("/incidentes", async (req, res) => {
+  const db = await criarBanco(); // Chamamos a função do outro arquivo. O await é o "aguarde", pois o banco precisa de tempo para abrir.
 
-const db = await criarBanco() // Chamamos a função do outro arquivo. O await é o "aguarde", pois o banco precisa de tempo para abrir. 
+  const listaIncidentes = await db.all(`SELECT * FROM incidentes`);
 
-const listaIncidentes = await db.all(`SELECT * FROM incidentes`)
-
-res.json(listaIncidentes) //Entrega esses dados para o cliente em formato JSON
-
-} ) 
+  res.json(listaIncidentes); //Entrega esses dados para o cliente em formato JSON
+});
 
 // rota especifica
 
-app.get("/incidentes/:id", async (req,res) => {
+app.get("/incidentes/:id", async (req, res) => {
+  const { id } = req.params;
 
-  const { id } = req.params
+  const db = await criarBanco();
 
-  const db = await criarBanco()
-
-  const incidenteEspecifico = await db.all(`SELECT * FROM incidentes WHERE id = ?` , [id])
+  const incidenteEspecifico = await db.all(
+    `SELECT * FROM incidentes WHERE id = ?`,
+    [id],
+  );
   // esse ? é um espaço reservado que será preenchido pelo valor da variavel [id]
 
-  res.json(incidenteEspecifico)
-
-})
+  res.json(incidenteEspecifico);
+});
 
 //Rota POST - Novos Registros /Endpoints
 
 app.post("/incidentes", async (req, res) => {
+  const {
+    tipo_problema,
+    localizacao,
+    descricao,
+    prioridade,
+    nome_solicitante,
+    data_registro,
+    hora_registro,
+  } = req.body;
 
-  const {tipo_problema, localizacao, descricao, prioridade, nome_solicitante, data_registro, hora_registro} = req.body
+  const db = await criarBanco();
 
-  const db = await criarBanco()
+  await db.run(
+    `INSERT INTO incidentes(tipo_problema, localizacao, descricao, prioridade, nome_solicitante, data_registro, hora_registro) VALUES (?, ?, ?, ?, ?, ?, ?)`,
+    [
+      tipo_problema,
+      localizacao,
+      descricao,
+      prioridade,
+      nome_solicitante,
+      data_registro,
+      hora_registro,
+    ],
+  );
 
-  await db.run(`INSERT INTO incidentes(tipo_problema, localizacao, descricao, prioridade, nome_solicitante, data_registro, hora_registro) VALUES (?, ?, ?, ?, ?, ?, ?)` , [tipo_problema, localizacao, descricao, prioridade, nome_solicitante, data_registro, hora_registro])
+  res.send(
+    `incidentes novo registrado: ${tipo_problema} registrado na data ${data_registro} por ${nome_solicitante}`,
+  );
+});
 
-  res.send(`incidentes novo registrado: ${tipo_problema} registrado na data ${data_registro} por ${nome_solicitante}`)
-})
+//Rota de atualização
+
+app.put("/incidentes/:id", async (req, res) => {
+  //pega o ID do incidente que vem pela URL (ex: /incidentes/4)
+  const { id } = req.params;
+
+  // pega os novos dados enviados no corpo da requisição (o que será atualizado)
+  const { descricao, prioridade, status_resolucao } = req.body;
+
+  //Abre a conexão com o banco de dados
+  const db = await criarBanco();
+
+  await db.run(
+    `
+    UPDATE incidentes 
+    SET descricao = ?,
+    prioridade = ?,
+    status_resolucao = ?
+    WHERE id = ?`,
+    [descricao, prioridade, status_resolucao, id],
+  );
+
+  //enviar uma resposta para o cliente
+  res.send(`O incidente de ${id} foi atualizada com sucesso `);
+});
+
+//Rota de remoção
+
+app.delete("/incidentes/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const db = await criarBanco();
+
+  await db.run(`
+    
+    DELETE FROM incidentes WHERE id = ?`,
+    [id],);
+
+  res.send(` O incidente de ${id} foi deletado com sucesso`);
+});
+
+
+//Porta do servidor
+
+//criando váriavel inteligente para a porta
+const PORT = process.env.PORT || 3000;
+
+//Ligando o Servidor
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+  
+});
